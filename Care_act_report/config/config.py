5f4aba39551b222e5edaaa05ef0ae95d8523d82b
Aravindhant07 <87820041+Aravindhant07@users.noreply.github.com>
2024-05-20 23:03:33 +0100

    commit

--- Care_act_report/config/config.py
+++ Care_act_report/config/config.py
@@ -1,28 +1,22 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
-load_dotenv()
-
-class Config:
-    # Configuration for file paths and settings
-    DETAILS_PATH = os.getenv('DETAILS_PATH')
-    BASE_DATA_PATH = os.getenv('BASE_DATA_PATH')
-    IMAGE_DIR = os.getenv('IMAGE_DIR')
-    REPORT_DIR = os.getenv('REPORT_DIR')
-    SMTP_SERVER = 'smtp.gmail.com'
-    SMTP_PORT = 587
-    SMTP_USER = 'aravindhant7@gmail.com'
-    SMTP_PASSWORD = os.getenv('SMTP_PASSWORD')
-    HEADER_IMAGE = os.getenv('HEADER_IMAGE')
-    FOOTER_IMAGE = os.getenv('FOOTER_IMAGE')
-    REPORT_TIMEFRAME = 'quarterly'
-    DIRECTORY = os.getenv('Directory')
-    LOG_DIR = os.getenv('Log')
-
-    @staticmethod
-    def ensure_directories():
-        """Ensure necessary directories exist."""
-        os.makedirs(Config.IMAGE_DIR, exist_ok=True)
-        os.makedirs(Config.REPORT_DIR, exist_ok=True)
-        os.makedirs(Config.LOG_DIR, exist_ok=True)
+class ConfigLoader:
+    def load_config(self):
+        load_dotenv()
+        return {
+            'details_path': os.getenv('DETAILS_PATH'),
+            'base_data_path': os.getenv('BASE_DATA_PATH'),
+            'image_dir': os.getenv('IMAGE_DIR'),
+            'report_dir': os.getenv('REPORT_DIR'),
+            'smtp_server': 'smtp.gmail.com',
+            'smtp_port': 587,
+            'smtp_user': 'aravindhant7@gmail.com',
+            'smtp_password': os.getenv('SMTP_PASSWORD'),
+            'header_image': os.getenv('HEADER_IMAGE'),
+            'footer_image': os.getenv('FOOTER_IMAGE'),
+            'report_timeframe': 'quarterly',
+            'Directory': os.getenv('Directory'),
+            'log_dir': os.getenv('Log'),
+            'log_file': os.path.join(os.getenv('Log'), "care_assessment_report.log")
+        }
--- Care_act_report/insights/insights_generator.py
+++ Care_act_report/insights/insights_generator.py
@@ -1,23 +1,39 @@
 from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-from langchain_community.chat_models import ChatOpenAI
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.embeddings import OpenAIEmbeddings
+import traceback
+import os
 
-class InsightsGenerator:
-    def __init__(self, retriever):
-        self.retriever = retriever
-        self.llm = ChatOpenAI(temperature=0.4, model_name='gpt-3.5-turbo')
+class InsightGenerator:
+    def __init__(self, config):
+        self.config = config
+        self.embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+        self.retriever = self._setup_retriever()
 
-    def generate_data_insights(self, df, use_rag=True):
-        """Generate insights for each column in the DataFrame."""
+    def _setup_retriever(self):
+        loader = DirectoryLoader(self.config['Directory'], show_progress=True)
+        documents = loader.load()
+        docs = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100).split_documents(documents)
+        vectordb = Chroma.from_documents(documents=docs, embedding=self.embeddings, persist_directory='db')
+        vectordb.persist()
+        return vectordb.as_retriever(search_kwargs={"k": 2})
+
+    def generate_insights(self, df):
         insights = {}
+        llm = ChatOpenAI(temperature=0.4, model_name='gpt-3.5-turbo')
+
         for column in df.columns.difference(['Timestamp']):
             data_points = df[column].dropna().tolist()
             if data_points:
                 prompt_template = """
                     Based on the following data for {column}:
                     - Scores: {data_points}
-
+                    
                     Please provide empathetic insights into how this aspect affects the person's well-being, suggesting potential areas of support and improvement. Make sure there is no grammatical errors. 
                     The name given for the data is not a person its their daily need. And also use scores while generating the insights mention the numbers in content.
                 """
@@ -25,23 +41,12 @@ class InsightsGenerator:
                 prompt = PromptTemplate(template=prompt_template, input_variables=["column", "data_points"])
                 prompt_with_values = prompt.format(column=column, data_points=data_points)
 
-                if use_rag:
-                    try:
-                        qa_chain = RetrievalQA.from_chain_type(
-                            llm=self.llm,
-                            chain_type="map_reduce",
-                            retriever=self.retriever,
-                            return_source_documents=True
-                        )
-                        result = qa_chain({"query": prompt_with_values})
-                        insights[column] = result['result']
-                    except Exception as e:
-                        print(f"Failed to generate insights using retrieval for {column} due to an error: {e}")
-                else:
-                    try:
-                        response = self.llm.ask(prompt_with_values, log_level="info")
-                        insights[column] = response
-                    except Exception as e:
-                        print(f"Failed to generate insights directly for {column} due to an error: {e}")
+                try:
+                    qa_chain = RetrievalQA.from_chain_type(llm=llm, chain_type="map_reduce", retriever=self.retriever, return_source_documents=True)
+                    result = qa_chain({"query": prompt_with_values})
+                    insights[column] = result['result']
+                except Exception as e:
+                    print(f"Failed to generate insights using retrieval for {column} due to an error: {e}")
+                    print(traceback.format_exc())
 
         return insights
